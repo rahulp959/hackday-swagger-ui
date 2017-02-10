5efbc565feac9c33850034e38cf6160c7db2b3fd
Rahul Parkar <rahul.parkar@wipro.com>
2017-02-10 16:13:55 +0000

    Changes

--- bundle.js
+++ bundle.js
@@ -70,7 +70,12 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-document.write("It works.");
+var swaggerUi = new SwaggerUi({
+  url: 'http://petstore.swagger.io/v2/swagger.json',
+  dom_id: 'swagger-ui-container'
+});
+
+swaggerUi.load();
 
 
 /***/ })
